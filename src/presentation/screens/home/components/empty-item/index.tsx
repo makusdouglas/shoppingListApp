5ec718af95d9fef84a6d56d4d0044a6259702a30
Markus Douglas <makusdouglas@gmail.com>
2023-08-22 02:08:31 -0300

    reafactor: emptyItem behavior

--- src/presentation/screens/home/components/empty-item/index.tsx
+++ src/presentation/screens/home/components/empty-item/index.tsx
@@ -2,11 +2,13 @@ import React from 'react';
 import * as S from './styles';
 import Feather from 'react-native-vector-icons/Feather';
 import {useTheme} from 'styled-components';
+import {View} from 'react-native';
 
 type EmptyItemProps = {
   onPress: () => void;
+  message?: string;
 };
-export const EmptyItem: React.FC<EmptyItemProps> = ({onPress}) => {
+export const EmptyItem: React.FC<EmptyItemProps> = ({onPress, message}) => {
   const {COLORS} = useTheme();
 
   return (
@@ -14,7 +16,7 @@ export const EmptyItem: React.FC<EmptyItemProps> = ({onPress}) => {
       <S.IconContainer>
         <Feather name="plus" color={COLORS.BACKGROUND} size={18} />
       </S.IconContainer>
-      <S.Text>Adicione um template</S.Text>
+      <S.Text>{message}</S.Text>
     </S.EmptyItemContainer>
   );
 };
