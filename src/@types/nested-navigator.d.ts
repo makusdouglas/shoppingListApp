3fb869c361fc53846941b1fdf6d229b258493a5b
Markus Douglas <makusdouglas@gmail.com>
2023-08-22 02:05:46 -0300

    reafactor: change nested-navigator behavior

--- src/@types/nested-navigator.d.ts
+++ src/@types/nested-navigator.d.ts
@@ -1,5 +1,5 @@
 import {ParamListBase} from '@react-navigation/native';
 
-declare type NeastedNavigator<T extends ParamListBase> = {
+declare type NeastedNavigator<T extends {}> = {
   [K in keyof T]: {screen: K; params?: T[K]};
 }[keyof T];
